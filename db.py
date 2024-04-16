52a283d773d9796eb82407a9a32dbfd9d2b891ca
Marília Pereira <msap@cesar.school>
2024-04-16 17:27:45 -0300

    fix: Rename variables in postDatabase module

--- db.py
+++ db.py
@@ -23,20 +23,18 @@ class postDatabase:
     def createDatabase(dataset_name):
         postDatabase.initialize()
         
-        db_name = dataset_name
-
         conn = psycopg2.connect(**postDatabase.params, dbname='postgres')
         conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
         cursor = conn.cursor()
 
-        cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s", (db_name,))
+        cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s", (dataset_name,))
         exists = cursor.fetchone()
         if not exists:
-            cursor.execute(f'CREATE DATABASE "{db_name}"') 
-            print(f"Banco de dados {db_name} criado com sucesso.")
+            cursor.execute(f'CREATE DATABASE "{dataset_name}"') 
+            print(f"Banco de dados {dataset_name} criado com sucesso.")
         else:
-            print(f"O banco de dados {db_name} já existe.")
+            print(f"O banco de dados {dataset_name} já existe.")
 
         cursor.close()
         conn.close()
@@ -45,9 +43,7 @@ class postDatabase:
     def insertData(dataset_name, cols_to_use, file_name):
         postDatabase.initialize()
 
-        db_name = dataset_name
-
-        conn = psycopg2.connect(**postDatabase.params, dbname=db_name)
+        conn = psycopg2.connect(**postDatabase.params, dbname=dataset_name)
         cursor = conn.cursor()
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS airbnb_data (
@@ -69,4 +65,4 @@ class postDatabase:
         cursor.close()
         conn.close()
 
-        print(f"Dados inseridos no banco {db_name} com sucesso.")
+        print(f"Dados inseridos no banco {dataset_name} com sucesso.")
